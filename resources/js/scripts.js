31f946aef9b40428de2a0cd7f150580a9cda4f36
Ejukes <99070051+Ejukes@users.noreply.github.com>
2022-04-14 12:27:21 +1000

    nav-js

---
+++ resources/js/scripts.js
@@ -0,0 +1,14 @@
+
+// moile nav
+
+function openNav() {
+
+let toggler = document.querySelector( n'.nav-prompt')
+
+let navigation = document.querySelector('.main-nav')
+
+toggler.onclink = function() {
+    navigation.classList.toggle('open')
+}
+
+}
